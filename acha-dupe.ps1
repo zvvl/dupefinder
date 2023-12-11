b4e57a5aa9efa9eba52c090164850aae71c16e07
zvvl <64607828+zvvl@users.noreply.github.com>
2023-12-11 20:32:36 -0300

    Add files via upload

---
+++ acha-dupe.ps1
@@ -0,0 +1,4 @@
+﻿$srcDir = read-host "Indique a pasta inicial para a busca"
+Get-ChildItem -Path $srcDir -File -Recurse | Group -Property Length `
+    | where { $_.Count -gt 1 } | select -ExpandProperty Group | Get-FileHash `
+    | Group -Property Hash | where { $_.count -gt 1 }| foreach { $_.Group | select Path, Hash } 
